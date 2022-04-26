3f55fa96adcd862befa6f3f247716832a25d230a
och <git@chadenas.de>
2022-04-27 00:23:51 +0200

    removed direct level selection on welcome screen

--- app/src/main/java/com/example/cpudefense/WelcomeActivity.kt
+++ app/src/main/java/com/example/cpudefense/WelcomeActivity.kt
@@ -11,18 +11,20 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-        prepareStageSelector()
     }
 
     override fun onActivityReenter(resultCode: Int, data: Intent?) {
         super.onActivityReenter(resultCode, data)
-        prepareStageSelector()
     }
 
     fun startGame(v: View)
     {
-        var intent = Intent(this, MainGameActivity::class.java)
-        val startLevel = findViewById<NumberPicker>(R.id.stageSelect)?.value ?: 1
+        val prefs = getSharedPreferences(getString(R.string.pref_filename), Context.MODE_PRIVATE)
+        val maxStage = prefs.getInt("MAXSTAGE", 1)
+        val currentStage = prefs.getInt("LASTSTAGE", 1)
+        val startLevel = currentStage
+
+        val intent = Intent(this, MainGameActivity::class.java)
         intent.putExtra("START_ON_STAGE", startLevel)
         intent.putExtra("CONTINUE_GAME", false)
         startActivity(intent)
@@ -30,27 +32,16 @@ class WelcomeActivity : AppCompatActivity() {
 
     fun continueGame(v: View)
     {
-        var intent = Intent(this, MainGameActivity::class.java)
+        val intent = Intent(this, MainGameActivity::class.java)
         intent.putExtra("CONTINUE_GAME", true)
         startActivity(intent)
     }
 
     fun startLevelSelection(v: View)
     {
-        var intent = Intent(this, LevelSelectActivity::class.java)
+        val intent = Intent(this, LevelSelectActivity::class.java)
         startActivity(intent)
     }
 
-    fun prepareStageSelector()
-    {
-        var prefs = getSharedPreferences(getString(R.string.pref_filename), Context.MODE_PRIVATE)
-        val maxStage = prefs.getInt("MAXSTAGE", 1)
-        val currentStage = prefs.getInt("LASTSTAGE", 1)
-        val stageSelector = findViewById(R.id.stageSelect) as NumberPicker
-        stageSelector.maxValue = maxStage+1
-        stageSelector.minValue = 1
-        stageSelector.value = currentStage+1
-        stageSelector.wrapSelectorWheel = false
-    }
 
 }
