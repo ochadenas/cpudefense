152fe4ec0c7ebb59236e2fc44c3ee36b95d5c961
ochadenas <github@chadenas.de>
2023-10-23 15:25:35 +0200

    changed appearance of restart game dialog (again)

--- app/src/main/java/com/example/cpudefense/WelcomeActivity.kt
+++ app/src/main/java/com/example/cpudefense/WelcomeActivity.kt
@@ -1,11 +1,13 @@
 package com.example.cpudefense
 
+import android.app.Dialog
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.ContextThemeWrapper
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -125,20 +127,28 @@ class WelcomeActivity : AppCompatActivity()
 
     fun startNewGame(@Suppress("UNUSED_PARAMETER") v: View)
     {
-        val builder = AlertDialog.Builder(ContextThemeWrapper(this, R.style.CustomAlertDialog))
-        builder.setMessage(getString(R.string.query_restart_game))
-                .setCancelable(true)
-                .setNegativeButton("Yes") { dialog, id ->
-                    val intent = Intent(this, MainGameActivity::class.java)
-                    intent.putExtra("RESET_PROGRESS", true)
-                    intent.putExtra("START_ON_STAGE", 1)
-                    intent.putExtra("CONTINUE_GAME", false)
-                    startActivity(intent)
-                    setupButtons()
-                }
-                .setPositiveButton("No") { dialog, id -> dialog.dismiss() }
-
-        val alert = builder.create()
-        alert.show()
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.layout_dialog_new_game)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCancelable(true)
+        dialog.findViewById<TextView>(R.id.question).text = resources.getText(R.string.query_restart_game)
+        val button1 = dialog.findViewById<Button>(R.id.button1)
+        val button2 = dialog.findViewById<Button>(R.id.button2)
+        button2?.text = resources.getText(R.string.yes)
+        button1?.text = resources.getText(R.string.no)
+        button2?.setOnClickListener {
+            dialog.dismiss()
+            val intent = Intent(this, MainGameActivity::class.java)
+            intent.putExtra("RESET_PROGRESS", true)
+            intent.putExtra("START_ON_STAGE", 1)
+            intent.putExtra("CONTINUE_GAME", false)
+            startActivity(intent)
+            setupButtons()
+            }
+        button1?.setOnClickListener { dialog.dismiss() }
+        dialog.show()
     }
 }
