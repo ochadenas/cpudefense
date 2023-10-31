7d385320e421ff7de7a6c2d2070c476cfdab3bc5
ochadenas <github@chadenas.de>
2023-10-31 21:22:48 +0100

    number conversion into B and kiB also works for negative numbers. Closes #58.

--- app/src/main/java/com/example/cpudefense/gameElements/Scoreboard.kt
+++ app/src/main/java/com/example/cpudefense/gameElements/Scoreboard.kt
@@ -42,19 +42,19 @@ class ScoreBoard(val game: Game): GameElement() {
     }
 
     fun informationToString(number: Int): String {
-        if (number < 512)
+        if (number < 512 && number > -512)
             return "%d bit".format(number)
         var bytes = number / 8
-        if (bytes < 1000)
+        if (bytes < 1000 && bytes > -1000)
             return "%d B".format(bytes)
         bytes /= 1024
-        if (bytes < 1000)
+        if (bytes < 1000 && bytes > -1000)
             return "%d KiB".format(bytes)
         bytes /= 1024
-        if (bytes < 1000)
+        if (bytes < 1000 && bytes > -1000)
             return "%d MiB".format(bytes)
         bytes /= 1024
-        if (bytes < 1000)
+        if (bytes < 1000 && bytes > -1000)
             return "%d GiB".format(bytes)
         bytes /= 1024
         return "%d TiB".format(bytes)
