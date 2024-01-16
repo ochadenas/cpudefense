05355ab71d5c4dd82a1e5b34967ac089fb9ce2dc
ochadenas <github@chadenas.de>
2024-01-16 22:50:02 +0100

    translations corrected

--- app/src/main/java/com/example/cpudefense/AboutActivity.kt
+++ app/src/main/java/com/example/cpudefense/AboutActivity.kt
@@ -7,11 +7,8 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.widget.Button
-import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 
 
 class AboutActivity : AppCompatActivity()
